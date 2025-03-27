3ae51b4c854b11f1e937a1aa34673251a3c10739
tytnpp <thanyatorn2101@gmail.com>
2025-03-27 22:14:33 +0700

    Edit navbar for using login and add sidebar menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # Redirect user to login page after logout
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
