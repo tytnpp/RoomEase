1f50b79753814a0cdff749517101820b17cbcf58
tytnpp <thanyatorn2101@gmail.com>
2025-04-11 16:19:35 +0700

    Edit for at that moment gonna be In use

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,26 +6,40 @@ class RoomsController < ApplicationController
   end
 
   def show
-  @room = Room.find(params[:id])
+    @room = Room.find(params[:id])
 
-  # Define time slots (9 AM to 6 PM)
-  start_time = Time.current.change(hour: 9, min: 0, sec: 0)
-  end_time = Time.current.change(hour: 18, min: 0, sec: 0)
-  @time_slots = []
+    # Define time slots (9 AM to 6 PM)
+    start_time = Time.current.change(hour: 9, min: 0, sec: 0)
+    end_time = Time.current.change(hour: 18, min: 0, sec: 0)
+    @time_slots = []
 
-  while start_time < end_time
-    slot_end_time = start_time + 1.hour
+    while start_time < end_time
+      slot_end_time = start_time + 1.hour
 
-    # Check for overlapping reservations that are still active or in the future
-    is_available = @room.reservations
-                        .where("start_time < ? AND end_time > ?", slot_end_time, start_time)
-                        .where("end_time > ?", Time.current) # Exclude past reservations
-                        .empty?
+      # Check for overlapping reservations (excluding past)
+      overlapping = @room.reservations
+                         .where("start_time < ? AND end_time > ?", slot_end_time, start_time)
+                         .where("end_time > ?", Time.current)
 
-    @time_slots << { start_time: start_time, end_time: slot_end_time, available: is_available }
-    start_time = slot_end_time
+      is_available = overlapping.empty?
+
+      # Check if current time is in this slot and there's a reservation (i.e. in progress)
+      is_in_progress = @room.reservations
+                            .where("start_time <= ? AND end_time > ?", Time.current, Time.current)
+                            .where("start_time = ? AND end_time = ?", start_time, slot_end_time)
+                            .exists?
+
+      @time_slots << {
+        start_time: start_time,
+        end_time: slot_end_time,
+        available: is_available,
+        in_progress: is_in_progress
+      }
+
+      start_time = slot_end_time
+    end
   end
-end
+
 
   def new
     @room = Room.new
