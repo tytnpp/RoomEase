102c119f1e10b75e14e8ce0b7b669e64611fc93e
tytnpp <thanyatorn2101@gmail.com>
2025-04-13 20:58:56 +0700

    edit format in test file

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -12,15 +12,15 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
   test "should get show with time slots" do
     travel_to Time.zone.local(2025, 4, 10, 10, 0, 0) do
       get room_url(@room)
-  
+
       assert_response :success
-  
+
       time_slots = assigns(:time_slots)
       assert_not_nil time_slots
-  
+
       assert_equal 9, time_slots.first[:start_time].hour
       assert_equal 18, time_slots.last[:end_time].hour
-  
+
       time_slots.each do |slot|
         assert slot.key?(:available), "Slot missing :available"
         assert slot.key?(:in_progress), "Slot missing :in_progress"
@@ -29,4 +29,3 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 end
-  
