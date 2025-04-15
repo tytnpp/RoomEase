356b5451695c6dab948195335fd852ba3d8ff82b
tytnpp <thanyatorn2101@gmail.com>
2025-04-15 21:53:01 +0700

    edit migration file for debug add version

--- db/migrate/20250325013617_create_reservations.rb
+++ db/migrate/20250325013617_create_reservations.rb
@@ -1,4 +1,4 @@
-class CreateReservations < ActiveRecord::Migration[]
+class CreateReservations < ActiveRecord::Migration[7.1]
   def change
     create_table :reservations do |t|
       t.references :user, null: false, foreign_key: true
